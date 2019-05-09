01762b09d58a55d6c730f7440a55849994fdb981
Patrick Marre <pmarre40@gmail.com>
2019-05-09 07:06:15 -0700

    cleaned up code

--- portjs.js
+++ portjs.js
@@ -1,49 +1,48 @@
-$(document).ready(function () {
+$(document).ready(function() {
     // More Info Slider
     if ($(window).width() < 800) {
-        $(document).click(function(e) {
-            let target = e.target;
-            if (target.className === 'more_link') {
-                e.preventDefault();
-                for (let i = 0; i<15; i++) {
-                    if (target.id === 'sideBar-' + [i]) {
-                        $('#moreInfo-' + [i]).slideDown(300);
-                    } 
-                }
-            } else if (target.className === 'close') {
-                $('.moreInfo').slideUp(300);
+      $(document).click(function(e) {
+        let target = e.target;
+        if (target.className === 'more_link') {
+          e.preventDefault();
+          for (let i = 0; i < 15; i++) {
+            if (target.id === 'sideBar-' + [i]) {
+              $('#moreInfo-' + [i]).slideDown(300);
             }
-        });
-    } else if ($(window).width() >= 800){
-        $(document).click(function(e) {
-            let target = e.target;
-            if (target.className === 'more_link') {
-                e.preventDefault();
-                for (let i = 0; i<15; i++) {
-                    if (target.id === 'sideBar-' + [i]) {
-                        $('#moreInfo-' + [i]).slideDown(300);
-                    } 
-                }
-            } else if (target.className !== 'moreInfo') {
-                $('.moreInfo').slideUp(300);
+          }
+        } else if (target.className === 'close') {
+          $('.moreInfo').slideUp(300);
+        }
+      });
+    } else if ($(window).width() >= 800) {
+      $(document).click(function(e) {
+        let target = e.target;
+        if (target.className === 'more_link') {
+          e.preventDefault();
+          for (let i = 0; i < 15; i++) {
+            if (target.id === 'sideBar-' + [i]) {
+              $('#moreInfo-' + [i]).slideDown(300);
             }
-        });
+          }
+        } else if (target.className !== 'moreInfo') {
+          $('.moreInfo').slideUp(300);
+        }
+      });
     }
-
-// Mobile Menu Animations
-
-    $('.menu-stack').click(function(){
-        $('.mobile-overlay').css('display', 'block');
+  
+    // Mobile Menu Animations
+  
+    $('.menu-stack').click(function() {
+      $('.mobile-overlay').css('display', 'block');
     });
-    
-    $('.mobile-close').click(function(){
-        $('.mobile-overlay').css('display', 'none');
+  
+    $('.mobile-close').click(function() {
+      $('.mobile-overlay').css('display', 'none');
+    });
+  
+    $('.mobile-navItem').click(function() {
+      $('.mobile-overlay').css('display', 'none');
     });
 
-    $('.mobile-navItem').click(function(){
-        $('.mobile-overlay').css('display', 'none');
-    });
-
-
-
-});
+  });
+  
